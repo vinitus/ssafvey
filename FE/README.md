48a2b46bb9f3bdf8ac4ee106f3bc7a629bd41ed7
vinitus <tlsdnrng@gmail.com>
2023-04-13 12:45:51 +0900

    chore:project init and initial setting
    프로젝트 생성 환경
    1. yarn
    2. vite
    3. typescript + react
    4. tailwindcss
    5. concurrently
    
    package.json 추가 설정
    1. yarn dev -> 기존의 yarn vite + tailwindcss 관련 설정
    2. yarn build -> 기존의 tsc && vite build + tailwindcss 관련 설정
    3. yarn ac 파일명 -> git add 파일명 + commit을 한번에
    4. yarn head0 ~ head3 -> git reset --hard HEAD ~ HEAD^^^
    
    자세한건 README 참조

---
+++ FE/README.md
@@ -0,0 +1,43 @@
+### yarn ac 파일명
+
+git add 와 commit 한번에 하기
+
+.gitvscode로 커밋내용 저장하는 거면
+
+yarn ac 파일명
+
+git add 파일명 이 실행되고
+git commit 이 실행되어
+.gitvscode이 열려서 평소처럼 저장하고 닫으면 commit!
+
+yarn ac만 쓰면 git add .과 같음
+
+### yarn dev
+
+기존의 yarn dev시 yarn vite가 실행되어 로컬에서 서버가 열림.
+
+tailwindcss를 적용하려면
+
+```
+npx tailwindcss -i ./src/index.css -o ./src/output.css --watch
+```
+
+를 통해서 tailwind css에서 사용되는 클래스를 output.css에 적용해야함
+
+concurrently를 활용하여 둘 다 yarn dev로 실행되게 추가했음
+
+### head0 ~ head3
+
+git reset --hard HEAD
+
+0이면 HEAD, 1이면 HEAD1
+
+### yarn build
+
+tailwindcss 적용까지 하는 구문
+
+### output.css는 gitignore 하였음
+
+순서가 자꾸 바뀌기에 conflict가 자주 남
+
+yarn dev로 통일하였으니 괜찮음
---
+++ FE/tools/gitadd.js
@@ -0,0 +1,50 @@
+import { exec } from "child_process";
+const filename = process.argv[2];
+
+if (!filename) {
+  exec(`git add .`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`git add fail: ${error.message}`);
+      return;
+    }
+    if (stderr && stderr.split(":")[0] != "warning") {
+      console.error(`git add fail: ${stderr}`);
+      return;
+    }
+    console.log("git add success: ", filename);
+    exec("git commit", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`git commit fail: ${error.message}`);
+        return;
+      }
+      if (stderr && stderr.split(":")[0] != "warning") {
+        console.error(`git commit fail: ${stderr}`);
+        return;
+      }
+      console.log("git commit done: ", filename);
+    });
+  });
+}
+
+exec(`git add ${filename}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`git add fail: ${error.message}`);
+    return;
+  }
+  if (stderr && stderr.split(":")[0] != "warning") {
+    console.error(`git add fail: ${stderr}`);
+    return;
+  }
+  console.log("git add success: ", filename);
+  exec("git commit", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`git commit fail: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`git commit fail: ${stderr}`);
+      return;
+    }
+    console.log("git commit done: ", filename);
+  });
+});
