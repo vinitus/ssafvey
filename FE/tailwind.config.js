a9880c157c4c96f0f6534752fd539b0f92c7efc0
vinitus <tlsdnrng@gmail.com>
2023-04-18 13:40:24 +0900

    chroe:tailwind css init setting
    테일윈드 관련한 설정
    1. px단위도 미리 넣어두었습니다.
    2. main 컬러는 우리의 파란색 메인 컬러입니다. -> bg-main
    3. clearMain은 투명한 유리판 같은 컬러입니다.
    
    clearMain 사용시 bg-clearMain/30으로 opacity를 줘야합니다.

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
+const px0To100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0To200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
+// const px0To300 = { ...Array.from(Array(301)).map((_, i) => `${i}px`) };
+// const px0To400 = { ...Array.from(Array(401)).map((_, i) => `${i}px`) };
+// const px0To500 = { ...Array.from(Array(501)).map((_, i) => `${i}px`) };
+// const px0To600 = { ...Array.from(Array(601)).map((_, i) => `${i}px`) };
+// const px0To700 = { ...Array.from(Array(701)).map((_, i) => `${i}px`) };
+// const px0To800 = { ...Array.from(Array(801)).map((_, i) => `${i}px`) };
+// const px0To900 = { ...Array.from(Array(901)).map((_, i) => `${i}px`) };
+// const px0To1000 = { ...Array.from(Array(1001)).map((_, i) => `${i}px`) };
+// const px0To1100 = { ...Array.from(Array(1101)).map((_, i) => `${i}px`) };
+// const px0To1200 = { ...Array.from(Array(1201)).map((_, i) => `${i}px`) };
+// const px0To1300 = { ...Array.from(Array(1301)).map((_, i) => `${i}px`) };
+const px0To1400 = { ...Array.from(Array(1401)).map((_, i) => `${i}px`) };
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        main: 'linear-gradient(180deg, #00C6FB 0%, #1B6BB7 100%, #005BEA 100%)',
+      },
+      colors: {
+        clearMain: '#B0C1ED',
+      },
+      spacing: {
+        ...px0To1400,
+      },
+      fontSize: px0To200,
+      lineHeight: px0To100,
+      borderRadius: px0To100,
+    },
   },
   plugins: [],
 };
