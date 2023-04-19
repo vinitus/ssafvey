9dfe684d0a37996eea7885b2487ca7effd657813
subinKim <subin3277@gmail.com>
2023-04-19 14:03:05 +0900

    style:tailwind code put into module.css

--- FE/src/UI/HomeCard2.tsx
+++ FE/src/UI/HomeCard2.tsx
@@ -3,21 +3,21 @@ import style from './HomeCard.module.css'
 
 export default function HomeCard2() {
   return (
-    <div className="h-200 w-160 bg-main rounded-b-20 rounded-r-20 relative shadow-md shadow-black m-10">
-      <div className="h-200 w-160 bg-clearMain rounded-b-20 rounded-r-20 opacity-30 absolute -left-4 -top-4"/>
-      <div className="h-200 w-160 rounded-t-20 rounded-l-20 absolute -left-4 -top-4 text-white p-15 pt-25">
-        <div className='text-20 tracking-tighter text-left leading-24 break-keep'>
+    <div className={style.card2}>
+      <div className={style.background2}/>
+      <div className={style.content2}>
+        <div className={style.title}>
           IT 플랫폼 사용 실태 조사
         </div>
-        <div className='text-10 flex pt-15 pb-10'>
+        <div className={style.subtitle}>
           <img src="./public/icons/house.svg" alt='house' className='mr-5'/>
-          <div className='flex items-center'>
+          <div className={style.subtitle_writer}>
             기관명
           </div>
         </div>
-        <div className='text-10 flex pb-10'>
+        <div className={style.subtitle}>
           <img src="./public/icons/clock.svg" alt='clock' className='mr-5'/>
-          <div className="flex-none text-left">
+          <div className={style.subtitle_date}>
             <div>
               2023.04.10 -
             </div>
@@ -26,14 +26,14 @@ export default function HomeCard2() {
             </div>
           </div>
         </div>
-        <div className='text-10 flex'>
+        <div className={style.subtitle}>
           <img src="./public/icons/people.svg" alt='people' className='mr-5'/>
-          <div className='flex items-center'>
+          <div className={style.subtitle_writer}>
             100
           </div>
         </div>
-        <div className='text-10 absolute right-10 bottom-10'>
-          <div className='w-40 h-25 flex items-center justify-items-center bg-backwhite text-blue rounded-5'>
+        <div className={style.coin}>
+          <div className={style.coin_content}>
             <img src="./public/icons/coin.svg" alt='coin' className='-ml-8'/>
             <div className='ml-5'>100</div>
           </div>
