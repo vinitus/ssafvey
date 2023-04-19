e550ae931c5cdd7f9c2540803642de63086b79d9
benkim077 <123vfcx@gmail.com>
2023-04-19 13:46:25 +0900

    refact : semantic tag 적용
    - <div> 태그를 <header> 태그로 변경
    - 버튼 클래스 이름 변경 (arrowBackButton -> backButton)

--- FE/src/UI/AppBar.tsx
+++ FE/src/UI/AppBar.tsx
@@ -3,11 +3,11 @@ import style from './AppBar.module.css';
 
 export default function AppBar() {
   return (
-    <div className={style.appBarLayout}>
-      <button type="button" className={style.arrowBackButton}>
+    <header className={style.appBarLayout}>
+      <button type="button" className={style.backButton}>
         <img src="/icons/arrow_back.svg" alt="arrow_back" />
       </button>
       <img src="/logo.svg" alt="logo" className={style.logoSize} />
-    </div>
+    </header>
   );
 }
