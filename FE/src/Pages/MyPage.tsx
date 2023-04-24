cbd13c411111b4236a481bf88a2bf7f577c4ead6
vinitus <tlsdnrng@gmail.com>
2023-04-24 09:38:39 +0900

    feat:make module.css
    마이페이지 퍼블리싱 시작

--- FE/src/Pages/MyPage.tsx
+++ FE/src/Pages/MyPage.tsx
@@ -1,7 +1,6 @@
-export default function MyPage(){
-  return(
-    <div>
-      MyPage
-    </div>
-  )
+import React from 'react';
+import styles from './MyPage.module.css';
+
+export default function MyPage() {
+  return <div>MyPage</div>;
 }
