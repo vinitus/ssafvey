55b2e676f5f83038ed3f684df11da2a414975ebd
vinitus <tlsdnrng@gmail.com>
2023-04-24 15:43:49 +0900

    chore:remove /pulbic
    터미널 노란 경고 지워버리기

--- FE/src/Components/Home/HomeCard.tsx
+++ FE/src/Components/Home/HomeCard.tsx
@@ -1,56 +1,39 @@
 import React from 'react';
-import style from './HomeCard.module.css'
+import style from './HomeCard.module.css';
 
 export default function HomeCardRight() {
   return (
     <div className={style.card}>
-      
-      <div className={style.title}>
-        IT 플랫폼 사용 실태 조사
-      </div>
+      <div className={style.title}>IT 플랫폼 사용 실태 조사</div>
       <div className={style.container}>
-
         <div className={style.subtitle}>
-          <img src="./public/icons/house.svg" alt='house' className='mr-5'/>
-          <div className={style.subtitle_writer}>
-            기관명
-          </div>
+          <img src="/icons/house.svg" alt="house" className="mr-5" />
+          <div className={style.subtitle_writer}>기관명</div>
         </div>
 
         <div className={style.subtitle}>
-          <img src="./public/icons/people.svg" alt='people' className='mr-5'/>
-          <div className={style.subtitle_writer}>
-            100
-          </div>
+          <img src="/icons/people.svg" alt="people" className="mr-5" />
+          <div className={style.subtitle_writer}>100</div>
         </div>
 
         <div className={style.subtitle}>
-          <img src="./public/icons/clock.svg" alt='clock' className='mr-5'/>
+          <img src="/icons/clock.svg" alt="clock" className="mr-5" />
           <div className={style.subtitle_date}>
-            <div>
-              2023.04.10 -
-            </div>
-            <div>
-              2023.04.13
-            </div>
+            <div>2023.04.10 -</div>
+            <div>2023.04.13</div>
           </div>
         </div>
 
         <div className={style.subtitle}>
-          <img src="./public/icons/coin.svg" alt='people' className='mr-5'/>
-          <div className={style.subtitle_date}>
-            100
-          </div>
+          <img src="/icons/coin.svg" alt="people" className="mr-5" />
+          <div className={style.subtitle_date}>100</div>
         </div>
-        
       </div>
 
-      <div className='flex justify-end'>
+      <div className="flex justify-end">
         <div className={style.dosurvey}>
-          <div className={style.surveytitle}>
-            참여하기
-          </div>
-          <img src="./public/icons/dosurvey.svg" alt="참여하기"/>
+          <div className={style.surveytitle}>참여하기</div>
+          <img src="/icons/dosurvey.svg" alt="참여하기" />
         </div>
       </div>
     </div>
