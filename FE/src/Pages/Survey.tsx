26c544a8183ca36156c1f228b15825bf1d36df32
benkim077 <123vfcx@gmail.com>
2023-04-24 15:37:48 +0900

    feature : 설문지 커버 퍼블리싱 중

--- FE/src/Pages/Survey.tsx
+++ FE/src/Pages/Survey.tsx
@@ -8,8 +8,11 @@ const descArr = [
   { key: '포인트', value: `${200}` },
 ];
 
-const descMap = {
+const stateMap = {
   '설문 제목': 'Survey Title',
+  '설문 설명': `Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam iure molestias reprehenderit omnis
+  quod ipsa inventore quisquam iusto sunt unde? Voluptatibus labore quas a hic quibusdam molestiae
+  molestias laudantium sit.`,
   '조사 기관': 'SSAFY',
   '설문 기간': '2023.04.10 ~ 2023.04.13',
   '참여 인원': '100명',
@@ -20,26 +23,24 @@ export default function Survey() {
   return (
     <article className="text-white">
       <header className={style.header}>
-        <h1>{descMap['설문 제목']}</h1>
-        <div className={style.childInlineBlock}>
-          <img src="/icons/house.svg" alt="house-icon" />
-          <h2 className="text-[10px]">{descMap['조사 기관']}</h2>
-        </div>
-        <div className={style.childInlineBlock}>
-          <img src="/icons/clock.svg" alt="clock-icon" />
-          <h2 className="text-[10px]">{descMap['설문 기간']}</h2>
+        <h1 className={style.title}>{stateMap['설문 제목']}</h1>
+        <div className={style.mainDesc}>
+          <span className={style.childInlineBlock}>
+            <img src="/icons/house.svg" alt="house-icon" className="px-5" />
+            <h2 className="text-[10px]">{stateMap['조사 기관']}</h2>
+          </span>
+          <span className={style.childInlineBlock}>
+            <img src="/icons/clock.svg" alt="clock-icon" className="px-5" />
+            <h2 className="text-[10px]">{stateMap['설문 기간']}</h2>
+          </span>
         </div>
       </header>
       <main className={style.coverBackgroundImg}>
         <div className={style.coverBackgroundFilter}>
-          <div className={style.padding}>
+          <div className={style.sectionWrapper}>
             <section className={style.descBox}>
               <img src="/icons/about-circle-outline.svg" alt="about-icon" />
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam iure molestias reprehenderit omnis
-                quod ipsa inventore quisquam iusto sunt unde? Voluptatibus labore quas a hic quibusdam molestiae
-                molestias laudantium sit.
-              </p>
+              <p>{stateMap['설문 설명']}</p>
             </section>
             <div />
             <section className={style.stateBox}>
@@ -50,8 +51,11 @@ export default function Survey() {
                 </dl>
               ))}
             </section>
-            <section>
-              <button type="button">설문 참여</button>
+            <section className={style.buttons}>
+              <button type="button">미참여</button>
+              <button type="button" className={style.startSurveyBtn}>
+                설문 참여
+              </button>
               <button type="button">
                 <img src="/icons/share.svg" alt="share-icon" />
               </button>
