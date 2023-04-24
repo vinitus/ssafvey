661bdad2cb7f401ca0d1c92abc70d22daed3421d
vinitus <tlsdnrng@gmail.com>
2023-04-24 15:47:16 +0900

    chore:remove all public
    퍼블릭 퍼블릭 퍼블릭

--- FE/src/Components/Home/Recommend.tsx
+++ FE/src/Components/Home/Recommend.tsx
@@ -1,33 +1,30 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState } from 'react';
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 // import required modules
-import { Autoplay, Pagination, Navigation } from "swiper";
+import { Autoplay, Pagination, Navigation } from 'swiper';
 
+import style from './Recommend.module.css';
+import HomeCard from './HomeCard';
 
-import style from "./Recommend.module.css"
-import HomeCard from "./HomeCard"
-
-export default function Recommend(){
+export default function Recommend() {
   return (
     <div className={style.recommend}>
       <div className="relative">
         <div className={style.reco}>
-          <div className={style.rocotitle}>
-            추천 설문
-          </div>
-          <img src="./public/icons/twinkle.svg" alt="참여하기"/>
+          <div className={style.rocotitle}>추천 설문</div>
+          <img src="./icons/twinkle.svg" alt="참여하기" />
         </div>
       </div>
       <div className={style.swiper}>
         <Swiper
-          style = {{
-            'height': '340px'
+          style={{
+            height: '340px',
           }}
           spaceBetween={30}
           autoplay={{
@@ -39,13 +36,17 @@ export default function Recommend(){
           }}
           modules={[Autoplay, Pagination]}
         >
-          <SwiperSlide><HomeCard /></SwiperSlide>
-          <SwiperSlide><HomeCard /></SwiperSlide>
-          <SwiperSlide><HomeCard /></SwiperSlide>
+          <SwiperSlide>
+            <HomeCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <HomeCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <HomeCard />
+          </SwiperSlide>
         </Swiper>
-        
       </div>
-      
     </div>
-  )
+  );
 }
--- FE/src/Components/Search/Card.tsx
+++ FE/src/Components/Search/Card.tsx
@@ -1,38 +1,31 @@
-import React from "react"
-import style from "./Card.module.css"
+import React from 'react';
+import style from './Card.module.css';
 
-export default function Card(){
+export default function Card() {
   return (
     <div className={style.card2}>
-      <div className={style.background2}/>
+      <div className={style.background2} />
       <div className={style.content2}>
-        <div className={style.title}>
-          IT 플랫폼 사용 실태 조사
-        </div>
+        <div className={style.title}>IT 플랫폼 사용 실태 조사</div>
         <div className={style.subtitle}>
-          <img src="./public/icons/house.svg" alt='house' className='mr-5'/>
-          <div className={style.subtitle_writer}>
-            기관명
-          </div>
+          <img src="./icons/house.svg" alt="house" className="mr-5" />
+          <div className={style.subtitle_writer}>기관명</div>
         </div>
         <div className="flex items-start">
           <div className={style.subtitle}>
-            <img src="./public/icons/clock.svg" alt='clock' className='mr-5'/>
+            <img src="./icons/clock.svg" alt="clock" className="mr-5" />
             <div className={style.subtitle_date}>
-              <div>
-                2023.04.10 - 2023.04.13
-              </div>
+              <div>2023.04.10 - 2023.04.13</div>
             </div>
           </div>
-
         </div>
         <div className={style.coin}>
           <div className={style.coin_content}>
-            <img src="./public/icons/coin_color.svg" alt='coin' className='-ml-8'/>
-            <div className='ml-5'>100</div>
+            <img src="./icons/coin_color.svg" alt="coin" className="-ml-8" />
+            <div className="ml-5">100</div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
