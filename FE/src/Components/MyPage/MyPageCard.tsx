327511345389763fc60ce0556760c54c30bbc9c2
vinitus <tlsdnrng@gmail.com>
2023-04-24 13:07:50 +0900

    feat:done card publishing
    마이페이지의 포인트 같은 것 나타내주는 카드 컴포넌트
    퍼블리싱 끝

--- FE/src/Components/MyPage/MyPageCard.tsx
+++ FE/src/Components/MyPage/MyPageCard.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import styles from './MyPageCard.module.css';
 
-export default function MyPageCard() {
-  return <div>MyPageCard</div>;
+interface Props {
+  tag: '포인트' | '참여설문' | '제작설문';
+  quantity: number;
+}
+
+// export default function MyPageCard(props: Props) {
+//   const { tag, quantity } = props;
+export default function MyPageCard({ tag, quantity }: Props) {
+  let imgSrc = 'madeSurvey';
+  if (tag === '포인트') imgSrc = 'point';
+  else if (tag === '참여설문') imgSrc = 'joinedSurvey';
+  return (
+    <div className={styles.cardBgColor}>
+      <div className={styles.cardText}>{tag}</div>
+      <img src={`./icons/${imgSrc}.svg`} alt={tag} className={styles.img} />
+      <div className={styles.cardQuantity}>{quantity}</div>
+    </div>
+  );
 }
