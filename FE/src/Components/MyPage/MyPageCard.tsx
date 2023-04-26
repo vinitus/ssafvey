14378e25510a8e8e9794bad1d1f885a3b1165c08
vinitus <tlsdnrng@gmail.com>
2023-04-26 17:36:25 +0900

    bug:fix type error
    마이페이지에서 모달을 여는 상태들을 관리하고 있습니다.
    이 상태는 default는 false이지만, 버튼들을 클릭하면 해당되는 값으로 업데이트됩니다.
    여기서 할당될 값들의 타입들을 지정하기 위해서 ContentType를 선언하였습니다.
    또한, 기존의 이미지 위치를 매핑시켜주는 Object를 업데이트하여,
    MappingDataWithTag으로 바꾸고, key는 tag이름, value는 배열형태입니다.
    value에는 이미지 파일 이름과 모달을 오픈하기 위한 상태의 dispatch 함수에
    ContentType의 타입들로 변환하기 위해서입니다.

--- FE/src/Components/MyPage/MyPageCard.tsx
+++ FE/src/Components/MyPage/MyPageCard.tsx
@@ -4,31 +4,40 @@ import styles from './MyPageCard.module.css';
 interface Props {
   tag: '포인트' | '참여설문' | '제작설문';
   quantity: number;
-  modalOpenFunc?: React.Dispatch<React.SetStateAction<string | boolean>>;
+  modalOpenFunc?: React.Dispatch<React.SetStateAction<'응답한' | '제작한' | '쿠폰' | boolean>>;
 }
 
+interface MappingDataWithTag {
+  포인트: [string, false];
+  참여설문: [string, '응답한'];
+  제작설문: [string, '제작한'];
+}
+
+type ContentType = boolean | '응답한' | '제작한';
+
 export default function MyPageCard({ tag, quantity, modalOpenFunc }: Props) {
-  const tagToImgSrc = {
-    포인트: 'point',
-    참여설문: 'joinedSurvey',
-    제작설문: 'madeSurvey',
+  const mappingDataWithTag: MappingDataWithTag = {
+    포인트: ['point', false],
+    참여설문: ['joinedSurvey', '응답한'],
+    제작설문: ['madeSurvey', '제작한'],
   };
 
-  const imgSrc = tagToImgSrc[tag];
+  const imgSrc = mappingDataWithTag[tag][0];
+  const contentType: ContentType = mappingDataWithTag[tag][1];
 
   return (
     <button
       type="button"
       disabled={tag === '포인트'}
       onClick={() => {
-        if (modalOpenFunc) modalOpenFunc(tag);
+        if (modalOpenFunc && contentType) modalOpenFunc(contentType);
       }}
     >
-      <div className={styles.cardBgColor}>
-        <div className={styles.cardText}>{tag}</div>
+      <article className={styles.cardBgColor}>
+        <h1 className={styles.cardText}>{tag}</h1>
         <img src={`./icons/${imgSrc}.svg`} alt={tag} className={styles.img} />
-        <div className={styles.cardQuantity}>{quantity}</div>
-      </div>
+        <h1 className={styles.cardQuantity}>{quantity}</h1>
+      </article>
     </button>
   );
 }
