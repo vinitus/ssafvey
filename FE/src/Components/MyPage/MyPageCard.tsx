3ddd67bf5e25be7f04ea8a5efc84435cb2dfb0e9
vinitus <tlsdnrng@gmail.com>
2023-04-26 09:24:59 +0900

    feat:wrap div with button tag based on condition
    1. 포인트를 나타내는 카드 컴포넌트는 상호작용이 불가하기에
    disabled를 조건문으로 추가하는 형식으로 작성
    2. defaultProps를 통해서 modalOpenHandler를 옵셔널 타입으로 만듬

--- FE/src/Components/MyPage/MyPageCard.tsx
+++ FE/src/Components/MyPage/MyPageCard.tsx
@@ -4,19 +4,35 @@ import styles from './MyPageCard.module.css';
 interface Props {
   tag: '포인트' | '참여설문' | '제작설문';
   quantity: number;
+  modalOpenFunc?: React.Dispatch<React.SetStateAction<string | boolean>>;
 }
 
-// export default function MyPageCard(props: Props) {
-//   const { tag, quantity } = props;
-export default function MyPageCard({ tag, quantity }: Props) {
-  let imgSrc = 'madeSurvey';
-  if (tag === '포인트') imgSrc = 'point';
-  else if (tag === '참여설문') imgSrc = 'joinedSurvey';
+export default function MyPageCard({ tag, quantity, modalOpenFunc }: Props) {
+  const tagToImgSrc = {
+    포인트: 'point',
+    참여설문: 'joinedSurvey',
+    제작설문: 'madeSurvey',
+  };
+
+  const imgSrc = tagToImgSrc[tag];
+
   return (
-    <div className={styles.cardBgColor}>
-      <div className={styles.cardText}>{tag}</div>
-      <img src={`./icons/${imgSrc}.svg`} alt={tag} className={styles.img} />
-      <div className={styles.cardQuantity}>{quantity}</div>
-    </div>
+    <button
+      type="button"
+      disabled={tag === '포인트'}
+      onClick={() => {
+        if (modalOpenFunc) modalOpenFunc(tag);
+      }}
+    >
+      <div className={styles.cardBgColor}>
+        <div className={styles.cardText}>{tag}</div>
+        <img src={`./icons/${imgSrc}.svg`} alt={tag} className={styles.img} />
+        <div className={styles.cardQuantity}>{quantity}</div>
+      </div>
+    </button>
   );
 }
+
+MyPageCard.defaultProps = {
+  modalOpenFunc: undefined,
+};
