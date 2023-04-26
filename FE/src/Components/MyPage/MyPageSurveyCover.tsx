13d4f1a7e161721764b1af52fc33a2f4f5ff13d4
vinitus <tlsdnrng@gmail.com>
2023-04-26 17:44:07 +0900

    refact:separate from the MyPageCover component
    MyPageCover 컴포넌트에서 분리한 것입니다.
    props 드릴링으로 데이터를 넘기고 있습니다.

---
+++ FE/src/Components/MyPage/MyPageSurveyCover.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { CoverData, isSurveyHistory } from './.myPageType';
+import style from './MyPageSurveyCover.module.css';
+
+export default function MyPageSurveyCover({ quantity, infoType, data }: CoverData) {
+  return (
+    <>
+      <header className={style.coverHeaderWrapper}>
+        <h1 className={style.coverHeaderTitle}>{`${infoType} 설문`}</h1>
+        <p className={style.coverHeaderQuantity}>{quantity}</p>
+      </header>
+      <section className={style.historyWrapper}>
+        {isSurveyHistory(data) &&
+          data.map((history, idx) =>
+            idx + 1 < data.length ? (
+              <article className={style.historyBlockWithDay} key={history.day}>
+                <h2 className={style.historyDay}>{history.day}</h2>
+                {history.history.map(({ title, author }) => (
+                  <article className={style.historyBlock} key={title + author}>
+                    <h3 className={style.historyTitle}>{title}</h3>
+                    <div className={style.historyAuthorWrapper}>
+                      <img src="/icons/home.svg" alt="home" />
+                      <p className={style.historyAuthor}>{author}</p>
+                    </div>
+                  </article>
+                ))}
+              </article>
+            ) : (
+              <article key={history.day}>
+                <h2 className={style.historyDay}>{history.day}</h2>
+                {history.history.map(({ title, author }) => (
+                  <article className={style.historyBlock} key={title + author}>
+                    <h3 className={style.historyTitle}>{title}</h3>
+                    <div className={style.historyAuthorWrapper}>
+                      <img src="/icons/home.svg" alt="home" />
+                      <div className={style.historyAuthor}>{author}</div>
+                    </div>
+                  </article>
+                ))}
+              </article>
+            ),
+          )}
+      </section>
+    </>
+  );
+}
