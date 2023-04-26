824fa9401cb8e03e04cae35c21383a5e746f21ec
vinitus <tlsdnrng@gmail.com>
2023-04-26 09:28:49 +0900

    feat:make SurveyThumbnail component
    설문조사 객체를 간단하게 제목과 만든이로 나타내줄 컴포넌트

--- FE/src/Components/MyPage/SurveyThumbnail.tsx
+++ FE/src/Components/MyPage/SurveyThumbnail.tsx
@@ -3,16 +3,16 @@ import style from './SurveyThumbnail.module.css';
 
 interface Props {
   title: string;
-  madeBy: string;
+  author: string;
 }
 
-export default function SurveyThumbnail({ title, madeBy }: Props) {
+export default function SurveyThumbnail({ title, author }: Props) {
   return (
     <div className={style.surveyThumbnailWrapper}>
       <div className={style.surveyThumbnailTitle}>{title}</div>
-      <div className={style.surveyThumbnailMadeByWrapper}>
+      <div className={style.surveyThumbnailAuthorWrapper}>
         <img src="/icons/home.svg" alt="home" />
-        <div className={style.surveyThumbnailMadeByDiv}>{madeBy}</div>
+        <div className={style.surveyThumbnailAuthorDiv}>{author}</div>
       </div>
     </div>
   );
