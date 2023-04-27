3ee93f8ed538da5c29457bd1bc48f83733e24f0b
benkim077 <123vfcx@gmail.com>
2023-04-27 10:38:22 +0900

    refact: ImportExcel HTML, CSS 수정
    - 일부 div 태그 제거
    - 스타일을 .modules.css 파일로 내보냄

--- FE/src/Components/Create/ImportExcel.tsx
+++ FE/src/Components/Create/ImportExcel.tsx
@@ -5,25 +5,19 @@ import PlusButton from "../../UI/Survey/PlusButton";
 
 export default function ImportExcel() {
   return (
-    <div className={style.sectionsWrapper}>
+    <div className={style.sections}>
       <SurveyBox>
-        <div className="flex flex-col gap-50">
-          <div>
-            <h2 className="text-black text-20 font-bold">엑셀 파일을 업로드하세요!</h2>
-            <p className="text-gray-600">설문지를 바로 생성할 수 있어요</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <PlusButton size="lg" />
-          </div>
-          <div className="text-center">
-            <button type="button" className="rounded-full text-white bg-gray-400 border-none py-10 px-15">
-              엑셀 템플릿 다운로드
-            </button>
-          </div>
+        <h2 className={style.title}>엑셀 파일을 업로드하세요!</h2>
+        <p className={style.desc}>설문지를 바로 생성할 수 있어요</p>
+        <div className={style.buttonContainer}>
+          <PlusButton size="lg" /> 
         </div>
+        <button type="button" className={style.templateDownLoadButton}>
+          엑셀 템플릿 다운로드
+        </button>
       </SurveyBox>
       <section className={style.buttons}>
-        <button type="button" className="rounded-20 text-white bg-darkBlue border-none py-15 px-25 text-20 w-full">직접 문항 작성하기</button>
+        <button type="button" className={style.writeQuestionsButton}>직접 문항 작성하기</button>
       </section>
     </div>
   );
