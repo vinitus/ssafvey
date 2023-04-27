36d170d33e29707aea2779fdae60848628f750c7
vinitus <tlsdnrng@gmail.com>
2023-04-27 13:43:01 +0900

    refact:기프트 카드 재사용성을 위한 리팩토링
    - 1. 프롭스를 제품 제목과 포인트를 받게끔 추가했습니다.
    - 2. 포인트는 옵셔널로 줬고 포인트 프롭스가 없다면 렌더링되지 않게했어요.

--- FE/src/Components/Exchange/GiftCard.tsx
+++ FE/src/Components/Exchange/GiftCard.tsx
@@ -1,22 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from './GiftCard.module.css';
 
-export default function GiftCard() {
+// 지금 tmp 때문에 tmp를 받기도 합니다. 나중에 수정 필요해요.
+interface GiftCardProps {
+  productTitle: '상품명' | '커피' | '아이스티';
+  point?: number;
+}
+
+export default function GiftCard({ productTitle, point }: GiftCardProps) {
+  // 나중에 통일성을 위해 수정이 필요해요.
+  const mappingImgSrc = {
+    상품명: './tmpFile/tmp.jpg',
+    커피: '/icons/coffee.svg',
+    아이스티: '/icons/iceTea.svg',
+  };
+
+  const imgSrc = mappingImgSrc[productTitle];
 
   return (
     <div className={style.giftcard}>
       <div className={style.giftimg}>
-        <img src="./tmpFile/tmp.jpg" alt="tmp" />
+        <img src={imgSrc} alt="tmp" />
       </div>
 
-      <div className={style.gifttitle}>상품명</div>
+      <div className={style.gifttitle}>{productTitle}</div>
 
       <div className={style.coin}>
         <div className={style.coin_content}>
           <img src="./icons/coin_color.svg" alt="coin" className="-ml-8" />
-          <div className="ml-5">100</div>
+          <div className="ml-5">{point}</div>
         </div>
       </div>
     </div>
   );
 }
+
+GiftCard.defaultProps = {
+  point: undefined,
+};
