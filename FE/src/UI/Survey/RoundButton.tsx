cde44ac51a5a7d1b3e62547d0d2df5e85be77190
benkim077 <123vfcx@gmail.com>
2023-04-28 17:26:28 +0900

    refact: LeftArrowButton, Rigth 버튼 리팩토링
    - Left, Right 나누지 않고 Composition으로 구현
    - 추가 리팩토링 필요: button, a 태그가 겹쳤을 때 문제가 발생 -> div로 구현해야 하나?

--- FE/src/UI/Survey/RoundButton.tsx
+++ FE/src/UI/Survey/RoundButton.tsx
@@ -2,16 +2,17 @@ import React from "react";
 
 interface Props {
   hidden?: boolean;
+  children: React.ReactNode;
 }
 
-export default function RightArrowButton({ hidden }: Props) {
+export default function RoundButton({ hidden, children }: Props) {
   return (
     <button type="button" className={`${hidden ? "invisible" : ""} h-60 w-60 bg-darkBlue rounded-full`}>
-      &gt;
+      {children}
     </button>
-  );
+  )
 }
 
-RightArrowButton.defaultProps = {
+RoundButton.defaultProps = {
   hidden: false,
-};
+}
