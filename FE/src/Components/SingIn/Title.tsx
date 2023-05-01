f312146d415de4805c2299477eaa1f3b20eb34d5
subinKim <subin3277@gmail.com>
2023-05-01 14:35:58 +0900

    feature:signin

---
+++ FE/src/Components/SingIn/Title.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import style from "./Title.module.css"
+
+export default function Title() {
+  return(
+    <article className={style.signin}>
+
+      <div className={`${style.circle1} ${style.circle}`}/>
+      <div className={`${style.circle2} ${style.circleani}`}/>
+      <div className={`${style.circle3} ${style.circleani}`}/>
+      <div className={`${style.circle4} ${style.circle}`}/>
+      <div className={`${style.circle5} ${style.circle}`}/>
+
+      <header className={style.subtitle}>
+        <div>쉽게 <span>참여</span>하는</div>
+        <div>쉽게 <span>등록</span>하는</div>
+        <div>쉽게 <span>확인</span>하는</div>
+        <div>설문조사</div>
+      </header>
+      <main className={style.backgroundtitle}>
+        <div className={style.btmrect}>
+          <div className={style.solid}/>
+          <div className={style.rect}/>
+          <div className={style.dash}/>
+        </div>
+
+        <div className={style.toprect}>
+          <div className={style.dash}/>
+          <div className={style.rect}/>
+          <div className={style.solid}/>
+        </div>
+
+        <div className={style.title}>
+          SSAFVEY
+        </div>
+      </main>
+
+      <div className={style.kakao}>
+        <img src="/icons/kakao_login_medium_wide.png" alt="kakao"/>
+      </div>
+      
+    </article>
+  )
+}
