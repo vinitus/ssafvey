bc636683e1b25c161cb90827696895c722c0603f
YouYoungLee <dldbdud111@gmail.com>
2023-05-04 17:16:46 +0900

    :memo:

--- BE/ssafvey/src/main/java/com/ssafy/ssafvey/global/config/jwt/JwtFilter.java
+++ BE/ssafvey/src/main/java/com/ssafy/ssafvey/global/config/jwt/JwtFilter.java
@@ -20,7 +20,7 @@ public class JwtFilter extends GenericFilterBean {
    // logger 선언
    private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
    // ACCESS_HEADER 선언
-   public static final String ACCESS_HEADER = "accessToken";
+   public static final String ACCESS_HEADER = "Authorization";
    // REFRESH_HEADER 선언
    public static final String REFRESH_HEADER = "refreshToken";
    private TokenProvider tokenProvider;
