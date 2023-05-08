0d4b4894875c0212a3dcd6d5e6c316ebb691cbc6
vinitus <tlsdnrng@gmail.com>
2023-05-08 12:42:50 +0900

    feat:surveyData가 전역적으로 사용되어 만듬
    컴포넌트 자체와 요청하는 axios 인스턴스에서도 사용되어요.

---
+++ FE/src/types/surveyType.ts
@@ -0,0 +1,9 @@
+export interface surveyData {
+  isDone: boolean;
+  title: string;
+  description: string;
+  creator: string;
+  endData: string;
+  targetSurveyParticipants: number;
+  lotto: number;
+}
