9e83c21de7ff4fd225690c8ef5d922c80a7a8dec
YouYoungLee <dldbdud111@gmail.com>
2023-05-10 15:08:06 +0900

    :memo: 이름 수정

--- BE/ssafvey/src/main/java/com/ssafy/ssafvey/domain/member/repository/JobsRepository.java
+++ BE/ssafvey/src/main/java/com/ssafy/ssafvey/domain/member/repository/JobsRepository.java
@@ -1,8 +1,7 @@
 package com.ssafy.ssafvey.domain.member.repository;
 
 import com.ssafy.ssafvey.domain.member.entity.Job;
-import com.ssafy.ssafvey.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JobRepository extends JpaRepository<Job, Long> {
+public interface JobsRepository extends JpaRepository<Job, Long> {
 }
