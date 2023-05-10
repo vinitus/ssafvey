0b1d46d3ebda86cd0540ebddf976bb8a791483c1
vinitus <tlsdnrng@gmail.com>
2023-05-10 21:17:43 +0900

    feat:질문 목록에 대한 타입 추가

--- FE/src/types/surveyType.ts
+++ FE/src/types/surveyType.ts
@@ -5,5 +5,22 @@ export interface SurveyCoverData {
   creator: string;
   endDate: string;
   targetSurveyParticipants: number;
+  surveyParticipants: number;
   lotto: number;
+}
+
+export interface SurveyQuestionData {
+  surveyQuestions: OrderObj[];
+}
+
+export interface OrderObj {
+  order: string;
+  question: string;
+  isMultipleChoice: string;
+  choices: ChoicesObj[];
+}
+
+export interface ChoicesObj {
+  order: number;
+  choice: string;
 }
