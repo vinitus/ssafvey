bac4f7c275434da729cee2ff993a10474ebcb0a6
vinitus <tlsdnrng@gmail.com>
2023-05-11 20:43:51 +0900

    feat:컴파운드 컴포넌트로 axios 요청까지는 보냅니다.
    클릭에 대한 CSS 처리와 버그처리가 완벽하진 않지만 1차 커밋

--- FE/src/UI/Survey/SurveyBox.tsx
+++ FE/src/UI/Survey/SurveyBox.tsx
@@ -1,10 +1,87 @@
 import React from 'react';
 import style from './SurveyBox.module.css';
+import { ChoicesObj } from '@/types/surveyType';
+import { AnswerObj } from '@/Components/Survey/hooks/useSurveyQuestionDataParser';
+import { go, map, curry } from '@/module/fx';
 
 interface Props {
   children: React.ReactNode;
 }
 
-export default function SurveyBox({ children }: Props) {
+interface AnswerProps {
+  isMultipleChoice: boolean;
+  choices: ChoicesObj[] | undefined;
+  order: number;
+  choiceObjState: string | number;
+  choiceStateDispatcher: React.Dispatch<React.SetStateAction<AnswerObj>>;
+}
+
+function SurveyBox({ children }: Props) {
   return <section className={style.question}>{children}</section>;
 }
+
+function isNumber(a: string) {
+  return Number.isNaN(Number(a)) ? a : Number(a);
+}
+
+function Question({ children }) {
+  return <h2 className={style.title}>{children}</h2>;
+}
+
+function Answer({ isMultipleChoice, choices, order, choiceObjState, choiceStateDispatcher }: AnswerProps) {
+  let renderComponent = <SingleAnswer order={order} />;
+  if (isMultipleChoice && choices && typeof choiceObjState === 'number')
+    renderComponent = <MultipleAnswer choices={choices} order={order} />;
+
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    const [choiceType, questionOrder, answerOrder] = go(id.split('-'), map(isNumber));
+
+    if (choiceType === 'choices')
+      choiceStateDispatcher((prev) => {
+        prev[questionOrder] = answerOrder;
+        return prev;
+      });
+    else
+      choiceStateDispatcher((prev) => {
+        prev[questionOrder] = value;
+        return prev;
+      });
+  };
+
+  const fieldChangeHandler = (e: React.ChangeEvent<HTMLFieldSetElement>) => {
+    if (e.target.tagName === 'INPUT') {
+      inputChangeHandler(e as unknown as React.ChangeEvent<HTMLInputElement>);
+    }
+  };
+
+  return (
+    <fieldset className={style.choices} onChange={fieldChangeHandler}>
+      {renderComponent}
+    </fieldset>
+  );
+}
+
+function MultipleAnswer({ order, choices, answer }: { order: number; choices: ChoicesObj[]; answer: number }) {
+  return (
+    <>
+      {choices.map((choice) => {
+        return (
+          <label key={choice.order} htmlFor={`choices-${order}-${choice.order}`} className={style.choice}>
+            <input type="radio" name="choices" id={`choices-${order}-${choice.order}`} className="invisible" />
+            {choice.choice}
+          </label>
+        );
+      })}
+    </>
+  );
+}
+
+function SingleAnswer({ order, answer }: { order: number; answer: string }) {
+  return <input type="text" name="choices" id={`choice-${order}-0`} className={style.choice} />;
+}
+
+SurveyBox.Question = Question;
+SurveyBox.Answer = Answer;
+
+export default SurveyBox;
