b0a8612920007fd307fee9249db01b0ee19d8a55
vinitus <tlsdnrng@gmail.com>
2023-05-12 16:37:44 +0900

    style:let eslint 에러 제거

--- FE/src/Components/Survey/module/tokenQuery.ts
+++ FE/src/Components/Survey/module/tokenQuery.ts
@@ -1,10 +1,8 @@
 import { QueryClient } from '@tanstack/react-query';
 
 const tokenQuery = async (queryClient: QueryClient) => {
-  let accessToken: string =
+  const accessToken: string =
     queryClient.getQueryData(['accessToken']) ?? (await queryClient.fetchQuery(['accessToken'], async () => 'tmp'));
-  if (accessToken == null) accessToken = 'tmp';
-
   return accessToken;
 };
 
