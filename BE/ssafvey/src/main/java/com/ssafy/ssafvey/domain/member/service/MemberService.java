c798ca7230ab3f4af2d21c4a10cdddf6bed5ce49
YouYoungLee <dldbdud111@gmail.com>
2023-05-12 16:06:04 +0900

    :memo: 포인트api추가

--- BE/ssafvey/src/main/java/com/ssafy/ssafvey/domain/member/service/MemberService.java
+++ BE/ssafvey/src/main/java/com/ssafy/ssafvey/domain/member/service/MemberService.java
@@ -31,4 +31,6 @@ public interface MemberService {
     Map<String, List<PointResponseDto>> getMypagePoint(Long id);
 
     Map<String,Object> tmpAccessToken(Long id);
+
+    MemberPointResponseDto getMemberPoint(Long id);
 }
