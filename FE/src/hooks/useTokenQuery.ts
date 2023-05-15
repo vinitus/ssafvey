21ae5a03c368892a35569cf596bbd8391ca2e10a
vinitus <tlsdnrng@gmail.com>
2023-05-15 17:13:21 +0900

    feat:리액트 쿼리의 액세스 토큰에 대한 커스텀 훅

---
+++ FE/src/hooks/useTokenQuery.ts
@@ -0,0 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+import { getRefresh } from '@/Api/member';
+
+interface Options {
+  onError: () => void;
+  onSuccess: (accessToken: string) => void;
+}
+
+export const useTokenQuery = (options?: Options) => {
+  const tokenQuery = useQuery(['accessToken'], {
+    queryFn: async () => {
+      const oldRefreshToken = localStorage.getItem('refreshToken');
+      if (!oldRefreshToken) return false;
+      const data = await getRefresh(oldRefreshToken);
+      const { Authorization, refreshToken } = data;
+
+      localStorage.setItem('refreshToken', refreshToken);
+
+      return Authorization;
+    },
+    staleTime: 1500000,
+    cacheTime: Infinity,
+    onError: () => (options && options.onError ? options.onError() : {}),
+    onSuccess: (accessToken) => (options && options.onSuccess ? options.onSuccess(accessToken) : {}),
+  });
+  return tokenQuery;
+};
