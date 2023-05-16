c0a9c9f8090db4fe93460af86715f1fd8ca64c7e
benkim077 <123vfcx@gmail.com>
2023-05-16 15:08:23 +0900

    feature: 설문 생성 페이지 들어갈 때, 토큰없으면 login 페이지로 라우팅

--- FE/src/Pages/CreateSurvey.tsx
+++ FE/src/Pages/CreateSurvey.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import SurveyHeader from '../Components/Survey/SurveyHeader';
+import { useTokenQuery } from '@/hooks/useTokenQuery';
 
 const HEADLINE = '설문지 만들기';
 
 export default function CreateSurvey() {
+  const navigate = useNavigate();
+
+  console.log(localStorage.getItem('refreshToken'));
+
+  const tokenQuery = useTokenQuery({
+    onError: () => {
+      console.log('onError');
+      localStorage.setItem('refreshToken', '');
+      navigate('/sign-in');
+    },
+    onSuccess: (accessToken) => {
+      console.log('onSuccess');
+      console.log(accessToken);
+    },
+  });
+
+  useEffect(() => {
+    const refreshToken = localStorage.getItem('refreshToken');
+    console.log(tokenQuery);
+    if (!refreshToken) {
+      navigate('/sign-in');
+    }
+    console.log(refreshToken); // 리프레시
+    console.log(tokenQuery.data); // 엑세스
+  }, [navigate, tokenQuery]);
+
   return (
     <article className="text-white">
       <SurveyHeader title={HEADLINE} />
