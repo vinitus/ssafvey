a5635460a4a2b866f27d6ea3e6b67edd43b4ebe8
benkim077 <123vfcx@gmail.com>
2023-05-17 09:34:34 +0900

    feature: Excel Export 함수 구현

---
+++ FE/src/Util/Xlsx/writeExcel.ts
@@ -0,0 +1,89 @@
+import * as XLSX from 'xlsx';
+
+interface SurveyQuestionStats {
+  order: number;
+  question: string;
+  isMultipleChoice: boolean;
+  multipleChoiceStatDtoList: MultipleChoiceStatDto[] | null;
+  descriptiveChoiceStatDtoList: DescriptiveChoiceStatDto[] | null;
+}
+
+interface MultipleChoiceStatDto {
+  order: number;
+  count: number;
+  description: string;
+}
+
+interface DescriptiveChoiceStatDto {
+  answer: string;
+}
+
+export const dummyParams: SurveyQuestionStats[] = [
+  {
+    order: 1,
+    question: '1text',
+    isMultipleChoice: true,
+    multipleChoiceStatDtoList: [
+      {
+        order: 1,
+        count: 5,
+        description: '1text',
+      },
+      {
+        order: 2,
+        count: 3,
+        description: '2text',
+      },
+    ],
+    descriptiveChoiceStatDtoList: null,
+  },
+  {
+    order: 2,
+    question: '2text',
+    isMultipleChoice: false,
+    multipleChoiceStatDtoList: null,
+    descriptiveChoiceStatDtoList: [
+      {
+        answer: 'i like apple',
+      },
+      {
+        answer: 'i like apple',
+      },
+    ],
+  },
+];
+
+export default function writeExcel(surveyStats: SurveyQuestionStats[]) {
+  // 2차원 배열을 생성
+  const multipleChoiceColumn = Array.from({ length: 5 }, (_, i) => `choice ${i + 1}`);
+  const column = ['order', 'question', 'isMultipleChoice', ...multipleChoiceColumn, 'descriptiveAnswers'];
+
+  // TODO: 객관식, 주관식 로직 코드분리
+  // eslint-disable-next-line array-callback-return, consistent-return
+  const data = surveyStats.map((q) => {
+    const { order, question, isMultipleChoice, multipleChoiceStatDtoList, descriptiveChoiceStatDtoList } = q;
+    if (multipleChoiceStatDtoList !== null) {
+      const mc = multipleChoiceStatDtoList.map((a) => a.count);
+      const 부족한 = Array.from({ length: 5 - mc.length }, () => '');
+      const newMC = [...mc, ...부족한];
+
+      return [order, question, isMultipleChoice, ...newMC, descriptiveChoiceStatDtoList];
+    }
+
+    if (descriptiveChoiceStatDtoList !== null) {
+      const mc = Array.from({ length: 5 }, () => '');
+
+      const newDC = descriptiveChoiceStatDtoList.map((a) => a.answer);
+      return [order, question, isMultipleChoice, ...mc, ...newDC];
+    }
+  });
+
+  const mergedData: any = [column, ...data];
+
+  const wb = XLSX.utils.book_new();
+  const ws = XLSX.utils.aoa_to_sheet(mergedData);
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  XLSX.writeFile(wb, 'survey.xlsx');
+}
+
+// writeExcel(dummyParams);
